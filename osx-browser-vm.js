cc098fdb9e9e0617c10a67dd06ca19d8c7f7f9b4
Jed Schmidt <where@jed.is>
2016-02-03 10:31:43 -0500

    stringify osascript at startup

--- osx-browser-vm.js
+++ osx-browser-vm.js
@@ -5,26 +5,7 @@ export const chrome = {run: runInBrowser, name: "Google Chrome"}
 export const canary = {run: runInBrowser, name: "Google Chrome Canary"}
 export const firefox = {} // https://bugzilla.mozilla.org/show_bug.cgi?id=5704
 
-function runInBrowser(code, cb) {
-  if (typeof code == 'function') code = `(${code}())`
-
-  code = code.replace(/'/g, "'\\''")
-
-  var command = `osascript -l JavaScript - '${this.name}' '${code}'`
-  var options = {encoding: 'utf8'}
-  var file = run.toString()
-
-  exec(command, options, onexec).stdin.end(file)
-
-  function onexec(err, stdout, stderr) {
-    if (err) cb(err)
-
-    try { cb(null, JSON.parse(stdout)) }
-    catch (err) { cb(err) }
-  }
-}
-
-function run(args) {
+const osascript = String(function run(args) {
   var appName = args[0]
   var javascript = args[1]
   var browser = Application(appName)
@@ -50,4 +31,22 @@ function run(args) {
 
   tab.close()
   return JSON.stringify(output)
+})
+
+function runInBrowser(code, cb) {
+  if (typeof code == 'function') code = `(${code}())`
+
+  code = code.replace(/'/g, "'\\''")
+
+  var command = `osascript -l JavaScript - '${this.name}' '${code}'`
+  var options = {encoding: 'utf8'}
+
+  exec(command, options, onexec).stdin.end(osascript)
+
+  function onexec(err, stdout, stderr) {
+    if (err) cb(err)
+
+    try { cb(null, JSON.parse(stdout)) }
+    catch (err) { cb(err) }
+  }
 }
